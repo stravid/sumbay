76a82207792bcaab3cb6b08929532203afc1979a
David Strauß <mail@stravid.com>
2011-09-05 20:41:37 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sumbay::Application.config.secret_token = '71335d94b1fe1c77ee8165a661c582cad244e99a632d110be6a0beabab057723468847943f256e27f7af84ceabeed192b4297471ab8e556d80f55e570731620b'
